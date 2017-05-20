0a2b210bc3cbb538f596e036a1569ed635f4bd26
SaculTP <lucaspaldan@gmail.com>
2017-05-20 12:47:57 +0200

    Add files via upload

---
+++ Hellopython.py
@@ -0,0 +1,41 @@
+import random
+import sys
+import os
+
+Uge_Dage = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag',
+            'Fredag', 'Lørdag', 'Søndag']
+
+print('1. dag i ugen er', Uge_Dage[0])
+print('2. dag i ugen er', Uge_Dage[1])
+print('3. dag i ugen er', Uge_Dage[2])
+print('4. dag i ugen er', Uge_Dage[3])
+print('5. dag i ugen er', Uge_Dage[4])
+print('6. dag i ugen er', Uge_Dage[5])
+print('7. dag i ugen er', Uge_Dage[6])
+
+print('\n')
+
+indkøbsliste = ['Tomater', 'Bacon', 'Æg', 'Vin',
+                'Special Øl', 'Grill Pølser', 'Applesin Juice']
+
+købe = "Jeg skal købe"
+
+print(købe, indkøbsliste[0])
+print(købe, indkøbsliste[1])
+print(købe, indkøbsliste[2])
+print(købe, indkøbsliste[3])
+print(købe, indkøbsliste[4])
+print(købe, indkøbsliste[5])
+print(købe, indkøbsliste[6w])
+
+print('\n')
+
+name = "Lucas"
+Kan_godt_lide = " Kan godt lide Is,"
+
+
+print(name, end= "")
+print(Kan_godt_lide, end= "")
+
+Is = " og han får tit is når det er varmt!"
+print(Is)
